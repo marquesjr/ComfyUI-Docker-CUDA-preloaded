5a5e7c766b72124cd031caab5d06a25841621b30
akitaonrails <fabioakita@gmail.com>
2025-04-22 12:13:26 -0300

    adding extra insightface files. inverting extensions to load before models in the entrypoint, so it creates directories before models download inside

--- init_scripts/entrypoint.sh
+++ init_scripts/entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-# 1) Models first
-echo "running init_models.sh"
-/usr/local/bin/init_models.sh "$@"
-
-# 2) Then extensions
 echo "running init_extensions.sh"
 /usr/local/bin/init_extensions.sh "$@"
 
-# 3) Finally launch ComfyUI
+echo "running init_models.sh"
+/usr/local/bin/init_models.sh "$@"
+
 echo "running the server"
 exec "$@"
